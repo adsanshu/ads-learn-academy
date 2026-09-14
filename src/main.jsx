d750a6bf0c87dee102f4e1cb6081f3b3e3d4efbf
adsanshu <adsanshu123@gmail.com>
2026-09-14 23:04:11 +0530

    Change React app rendering to static HTML
    Replaced React rendering with static HTML content.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+document.getElementById("root").innerHTML = `
+  <div style="
+    min-height:100vh;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    background:#0d6efd;
+    color:white;
+    font-size:30px;
+    font-weight:bold;
+  ">
+    REACT TEST
+  </div>
+`;
