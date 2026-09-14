50ad9b1f370636576eaf5926ca672767cb41d210
adsanshu <adsanshu123@gmail.com>
2026-09-14 23:07:34 +0530

    Refactor main.jsx to use ReactDOM and App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
-document.getElementById("root").innerHTML = `
-  <div style="
-    min-height:100vh;
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    background:#0d6efd;
-    color:white;
-    font-size:30px;
-    font-weight:bold;
-  ">
-    REACT TEST
-  </div>
-`;
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
